eba6643eb3919675c131dcf5d4516fe4e82d3dc2
Denny Schuldt <dschuldt@outlook.com>
2016-11-16 22:23:00 -0500

    Exploring webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const PATHS = {
+  app: path.join(__dirname, "app"),
+  build: path.join(__dirname, "assets")
+}
+
+module.exports = {
+  context: PATHS.app,
+  entry: path.join(PATHS.app, "js/index.js"),
+  output: {
+      path: PATHS.build,
+      publicPath: "/assets/",
+      filename: "js/index.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin(path.join("css/[name].css"))
+  ]
+};
