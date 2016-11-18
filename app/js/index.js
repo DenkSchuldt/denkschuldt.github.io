0c39851fc1747a2aa8cc0e42659cfcc45dc1ab9a
Denny Schuldt <dschuldt@outlook.com>
2016-11-17 23:26:10 -0500

    Some basic effects

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,34 @@
 import '../css/main.scss';
+import $ from 'jquery';
+
+
+$(document).ready(()=>{
+
+  updateContent("Hello World!");
+  updateHeight();
+  $(".background").css("backgroundColor", "white");
+  $(".content").fadeIn(
+    ()=>setTimeout(()=>$(".content").fadeOut(), 1000)
+  );
+
+  setTimeout(()=> {
+    updateContent("I'm Denny.");
+    updateHeight();
+    $(".content").fadeIn();
+  }, 1800)
+
+})
+
+/**
+ *
+ */
+const updateHeight = () => (
+  $(".content").css("marginTop", -$(".content").height()/2)
+)
+
+/**
+ *
+ */
+const updateContent = (str) => (
+  $(".content").html("<h1>"+str+"</h1>")
+)
