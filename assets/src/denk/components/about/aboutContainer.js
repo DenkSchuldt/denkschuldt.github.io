f065f6eb7a92880860dcb08fcff036c5dbff66b4
Denny Schuldt <dschuldt@outlook.com>
2018-10-06 11:55:44 -0500

    About fix

--- assets/src/denk/components/about/aboutContainer.js
+++ assets/src/denk/components/about/aboutContainer.js
@@ -17,7 +17,10 @@ class AboutContainer extends React.Component {
       <div className="about-container">
         <img src={Me}/>
         <div className="about">
-            Hi, I'm Denny K. Schuldt and I'm a Computer science graduate. I'm a knowledge driven developer with a special interest in UX and code quality. I like taking 360 photos to capture places I've been in. I really hope you find this page inspiring. Enjoy it!
+          ¡Hola! Soy Denny K. Schuldt y soy un Ingeniero en Ciencias Computacionales.
+          Me considero un desarrollador movido por el conocimiento, con un interés especial en UX y código de calidad.
+          Me gusta tomar fotos en 360 para capturar lugares en los que he estado.
+          Realmente espero que encuentres este sitio inspirador. ¡Disfrútalo!
         </div>
         <div className="about-social">
             <a href="https://github.com/DenkSchuldt" target="_blank">Github</a>
